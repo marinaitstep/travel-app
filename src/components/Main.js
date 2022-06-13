c4827f4e55672023bb1685db9430057e07da313e
marinaitstep <marinaitstep@gmail.com>
2022-06-13 03:20:35 +0300

    started search

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import {  Link } from "react-router-dom";
 import { TripList } from "./TripList";
 import Header from './Header';
 
 const Main = () => {
+const [title, setTitle] = useState('')
+const [duration, setDuration] = useState('')
+const [level, setLevel] = useState('')
+
+const handleChangeTitle = (e) => {
+    setTitle(e.target.value);
+    console.log(title);
+}
+const handleChangeDur = (e) => {
+  setDuration(e.target.value);
+  console.log(duration);
+}
+const handleChangeLev = (e) => {
+  setLevel(e.target.value);
+  console.log(level);
+}
     return(
       <>
       <Header nav={true}/>
@@ -13,11 +29,17 @@ const Main = () => {
         <form className="trips-filter__form" autocomplete="off">
           <label className="trips-filter__search input">
             <span className="visually-hidden">Search by name</span>
-            <input name="search" type="search" placeholder="search by title" />
+            <input name="search" type="search" placeholder="search by title" 
+            value = {title}
+            onChange={handleChangeTitle}
+            />
           </label>
           <label className="select">
             <span className="visually-hidden">Search by duration</span>
-            <select name="duration">
+            <select name="duration"
+            value = {duration}
+            onChange={handleChangeDur}
+            >
               <option value="">duration</option>
               <option value="0_x_5">&lt; 5 days</option>
               <option value="5_x_10">&lt; 10 days</option>
@@ -26,7 +48,10 @@ const Main = () => {
           </label>
           <label class="select">
             <span class="visually-hidden">Search by level</span>
-            <select name="level">
+            <select name="level"
+            value = {level}
+            onChange={handleChangeLev}
+            >
               <option value="">level</option>
               <option value="easy">easy</option>
               <option value="moderate">moderate</option>
@@ -40,6 +65,37 @@ const Main = () => {
       <ul className="trip-list">
           {TripList.map((data, key) => {
             let urlid = '/trip/'+data.id; 
+            if(title)
+            {
+              if(data.title.indexOf(title))
+              console.log('include')
+              else
+              console.log('not include')
+
+            }
+            if(duration==='0_x_5')
+            {
+              if(data.duration<5)
+              console.log('dur <5')
+
+            }
+            if(duration==='5_x_10')
+            {
+              if(data.duration>=5 && data.duration<10)
+              console.log('dur 5<10')
+
+            }
+            if(duration==='10_x')
+            {
+              if(data.duration>=10)
+              console.log('dur >=10')
+
+            }
+           
+              if(data.level===level)
+              console.log('leveltrue')
+
+        if(!title && !duration && !level){
         return(
           <>
           <li className="trip-card">
@@ -60,7 +116,8 @@ const Main = () => {
           <Link to={urlid} className="button">Discover a trip</Link>
           </li>
           </>
-        );  
+        )}
+        else return(<></>) 
         })}
       </ul>
       </section>
